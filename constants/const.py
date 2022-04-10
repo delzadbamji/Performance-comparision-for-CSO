f38c4a598b9e7ec234e834ca299374199f5ae385
Delzad Bamji <delzadbamji@gmail.com>
2022-04-10 16:29:35 -0400

    Create const.py

---
+++ constants/const.py
@@ -0,0 +1,12 @@
+D = 2
+number_of_trials = 30
+
+lower_bound = 0
+upper_bound = 0
+
+def set_lower_upper bound(lower_bound,upper_bound):
+  lower_bound = lower_bound
+  upper_bound = upper_bound
+  
+def get_lower_upper bound():
+  return lower_bound,upper_bound
